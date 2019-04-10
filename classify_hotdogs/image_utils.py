ce54b493f61cb9f2e4f481eeec1214cdcb24cc74
Ben Zhang <benzhang@tribalscale.com>
2019-04-10 17:13:58 -0400

    pad image to square with white during resize

--- classify_hotdogs/image_utils.py
+++ classify_hotdogs/image_utils.py
@@ -9,7 +9,7 @@ class ImageUtils:
   def resize_img(img):
     # resize and pad image to 224x224 for inception to use
     image_dim = 224
-    white = np.array([255, 255, 255])
+    WHITE = [255, 255, 255]
 
     (height, width, _) = img.shape
 
@@ -20,8 +20,13 @@ class ImageUtils:
     scale_factor = image_dim / (height if height > width else width)
     new_width = int(width * scale_factor)
     new_height = int(height * scale_factor)
+    pad_width = 0 if 224 - new_width < 0 else 224 - new_width
+    pad_height = 0 if 224 - new_height < 0 else 224 - new_height
+
     dim = (new_width, new_height)
 
     resized = cv2.resize(img, dim, cv2.INTER_AREA)
 
-    return resized
+    padded = cv2.copyMakeBorder(resized, 0, pad_height, 0, pad_width, cv2.BORDER_CONSTANT, value=WHITE)
+
+    return padded
