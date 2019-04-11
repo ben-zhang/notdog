b7b133553d2628e3bac23be581dddadc8e386e55
Ben <benzhang42@gmail.com>
2019-04-10 23:45:14 -0400

    WIP

--- classify_hotdogs/classifier.py
+++ classify_hotdogs/classifier.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy
 import os
-from .image_utils import resize_img
+from image_utils import ImageUtils
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -13,14 +13,20 @@ def initialize_dnn():
   model_path = os.path.join(inception_path, 'tensorflow_inception_graph.pb')
 
   class_names_descriptor = open(class_names_path, 'r')
-  class_names = class_names_descriptor.read()
+  class_names = class_names_descriptor.read().strip().split('\n')
 
   inception_net = cv2.dnn.readNetFromTensorflow(model_path)
+
+  print(class_names)
 
   return class_names, inception_net
 
 # Create confidence intervals for each bin in class_names for an image
 def classify_img(neural_net, image_path):
-  class_names, graph = neural_net
+  class_names, net = neural_net
 
   img = cv2.imread(image_path)
+
+  resized = resize_img(img)
+
+  inputBlob = 
